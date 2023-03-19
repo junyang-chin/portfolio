5c3b020ab7c73327ffe04fdc24a883e3ab56f8da
junyang-chin <danny_cjy@hotmail.com>
2023-03-19 15:26:48 +0800

    commit

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        light: "#f5ebe0",
+        dark: "#0a0a0a",
+      },
+      fontFamily: {
+        sans: ["Raleway", ...defaultTheme.fontFamily.sans],
+        title: ["Montserrat", "Helvetica", "sans"],
+      },
+    },
+  },
+  plugins: [],
+};
